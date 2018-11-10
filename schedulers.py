a33f1a084adda611e303d8061edeaf40d929a086
Marcell Pek <2333134p@student.gla.ac.uk>
2018-11-10 23:56:08 +0000

    SJF done

--- schedulers.py
+++ schedulers.py
@@ -8,7 +8,7 @@ class FCFS(SchedulerDES):
 	def scheduler_func(self, cur_event):
 		print("[{}]	Event arrives ({})".format(self.time, cur_event.event_type))
 		
-		# New event arrives.
+		# The only state we allow here is a new event, send it to the scheduler to run until it finishes.
 		if cur_event.event_type == EventTypes.PROC_ARRIVES:
 			print("[{}]	Process {} sent to dispatcher.".format(self.time, cur_event.process_id))
 			return self.processes[cur_event.process_id]
@@ -32,10 +32,29 @@ class FCFS(SchedulerDES):
 
 class SJF(SchedulerDES):
 	def scheduler_func(self, cur_event):
-		pass
+		print("[{}]	Event arrives ({})".format(self.time, cur_event.event_type))
+
+		if cur_event.event_type == EventTypes.PROC_ARRIVES:
+			valid_procs = filter(lambda proc: proc.process_state == ProcessStates.READY, self.processes)
+			best_proc = min(valid_procs, key=lambda proc: proc.service_time)
+
+			print("[{}]	Shortest process is {} with service time {}, sending to dispatcher.".format(self.time, best_proc.process_id, best_proc.service_time))
+			return best_proc
 
 	def dispatcher_func(self, cur_proc):
-		pass
+		print("[{}]	Dispatcher gets process #{}, in state {}, service time: {}".format(
+			self.time, cur_proc.process_id, cur_proc.process_state, cur_proc.service_time))
+
+		if cur_proc.process_state == ProcessStates.READY:
+			cur_proc.process_state = ProcessStates.RUNNING
+			cur_proc.run_for(cur_proc.service_time, self.time)
+			cur_proc.process_state = ProcessStates.TERMINATED
+
+			print("[{}]	Dispatcher ran process #{}, for {}.".format(self.time, cur_proc.process_id, cur_proc.service_time))
+
+			return Event(   process_id = cur_proc.process_id,
+					event_type=EventTypes.PROC_CPU_DONE,
+					event_time = self.time + cur_proc.service_time)
 
 class RR(SchedulerDES):
 	def scheduler_func(self, cur_event):
